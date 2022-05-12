9d9ce9c69be91d1935174935c22bfded81f1a97b
Aimee Truong <aimtruong@ucdavis.edu>
2022-05-11 19:54:38 -0700

    create client folder for development, installed concurrently to run both client and server together

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const { authMiddleware } = require('./utils/auth');
@@ -20,6 +21,15 @@ app.use(express.json());
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+  
+  // serve up static assets
+  if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
   
   db.once('open', () => {
     app.listen(PORT, () => {
