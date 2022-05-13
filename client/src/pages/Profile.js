2ebe05a81a5f453fdd5708585304e3474e2405db
Aimee Truong <aimtruong@ucdavis.edu>
2022-05-13 16:53:56 -0700

    add addThoughtForm section

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useParams, Navigate } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
+import ThoughtForm from '../components/ThoughtForm';
 
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
@@ -60,7 +61,7 @@ const Profile = (props) => {
           Add Friend
         </button>
         )}
-        
+
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -75,6 +76,9 @@ const Profile = (props) => {
             friends = {user.friends}
           />
         </div>
+      </div>
+      <div className = 'mb-3'>
+        {!userParam && <ThoughtForm />}
       </div>
     </div>
   );
